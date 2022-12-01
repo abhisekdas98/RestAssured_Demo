7cbd9c816eb2c5bf9a0368b268ac0e314f53b0e7
abhisekdas98 <abhisekdas98@gmail.com>
2022-12-01 20:56:20 +0530

    Complted Dynamic json with parameterized data linking

--- src/main/java/main/DynamicJson.java
+++ src/main/java/main/DynamicJson.java
@@ -5,17 +5,18 @@ import files.ReUsableMethod;
 import groovy.json.JsonParser;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 
 public class DynamicJson {
 
-    @Test
-    public void addBook()
+    @Test(dataProvider="BooksData")
+    public void addBook(String isbn,String aisle)
     {
         RestAssured.baseURI="http://216.10.245.166/";
         String response=given().header("Content-Type","application/json").
-                body(PayLoad.addBook("adsfc","2345")).log().all().
+                body(PayLoad.addBook(isbn,aisle)).log().all().
                 when().post("/Library/Addbook.php").then().log().all().statusCode(200).extract()
                 .response().asString();
         JsonPath js= ReUsableMethod.rawToJson(response);
@@ -24,6 +25,12 @@ public class DynamicJson {
 
         System.out.println(id);
 
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData()
+    {
+        return new Object[][]{{"ahz","456"},{"asdc","123"},{"ahashj","2844"}};
     }
 }
 
