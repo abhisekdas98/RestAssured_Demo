ff7cbd1220a2a88582733a4c5218ee8161c1b856
abhisekdas98 <abhisekdas98@gmail.com>
2022-12-01 21:11:13 +0530

    Added comments on how to add payload through doc path

--- src/main/java/main/DynamicJson.java
+++ src/main/java/main/DynamicJson.java
@@ -7,6 +7,12 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
 
 public class DynamicJson {
@@ -14,6 +20,10 @@ public class DynamicJson {
     @Test(dataProvider="BooksData")
     public void addBook(String isbn,String aisle)
     {
+        /*
+        we can also add json file through location of path of the json file provided by the developer
+        PayLoad.addBook(new String(Files.readAllBytes(Paths.get("provide path of json payload file"))));
+        */
         RestAssured.baseURI="http://216.10.245.166/";
         String response=given().header("Content-Type","application/json").
                 body(PayLoad.addBook(isbn,aisle)).log().all().
