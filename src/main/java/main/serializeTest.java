a622559694741bc87451d5ab163e937a32086c2f
abhisekdas98 <abhisekdas98@gmail.com>
2022-12-06 14:36:28 +0530

    added SpecBuilders Req and Resp specs

--- src/main/java/main/serializeTest.java
+++ src/main/java/main/serializeTest.java
@@ -6,8 +6,13 @@ import POJO.Location;
 import POJO.Place;
 import files.PayLoad;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.Assert;
 
 import java.util.LinkedList;
@@ -33,6 +38,20 @@ public class serializeTest {
         p.setPhone_number("+(91) 8328823423");
         p.setWebsite("www.abhisekdas98.com");
         p.setTypes(ll);
+
+        RequestSpecification rq=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+                .addQueryParam("key","qaclick123")
+                .setContentType(ContentType.JSON).build();
+
+       RequestSpecification res1=given().spec(rq).body(p);
+
+        ResponseSpecification respspec=new ResponseSpecBuilder().expectStatusCode(200)
+                .expectContentType(ContentType.JSON).build();
+
+
+        Response resp=res1.when().post("/maps/api/place/add/json").then().
+                spec(respspec).extract().response();
+
         Response res=given().queryParam("key","qaclick123").header("Content-Type","application/json")
             .body(p)
             .when().post("/maps/api/place/add/json")
@@ -40,6 +59,8 @@ public class serializeTest {
         String responseString=res.asString();
 
         System.out.println(responseString);
+
+        System.out.println(resp.asString());
     }
 }
 
